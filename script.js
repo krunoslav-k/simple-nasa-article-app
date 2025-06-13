3d0cae67289bd5149c0b28d19d7ef42d2fb7646c
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2025-06-13 23:06:51 +0200

    make 10 articles fetchable and renderable

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ async function fetchAndRenderArticlesData() {
   const responseData = await fetch(endpoint, { method: "GET" });
   articlesData = await responseData.json();
 
-  //console.log(articlesData.results);
+  console.log(articlesData.results);
 
-  const header2El = renderHeader2El(articlesData);
-  const imageEl = renderImageEl(articlesData);
-  const paragraphEl = renderParagraphEl(articlesData);
+  articlesData.results.forEach((article) => {
+    const header2El = renderHeader2El(article);
+    const imageEl = renderImageEl(article);
+    const paragraphEl = renderParagraphEl(article);
 
-  renderArticle(header2El, imageEl, paragraphEl);
+    renderArticle(header2El, imageEl, paragraphEl);
+  });
 }
 
 function renderArticle(header2El, imageEl, paragraphEl) {
@@ -23,19 +25,19 @@ function renderArticle(header2El, imageEl, paragraphEl) {
 
 function renderHeader2El(articlesData) {
   const header2El = document.createElement("h2");
-  header2El.textContent = articlesData.results[1].title;
+  header2El.textContent = articlesData.title;
   return header2El;
 }
 
 function renderImageEl(articlesData) {
   const imageEl = document.createElement("img");
-  imageEl.src = articlesData.results[0].image_url;
+  imageEl.src = articlesData.image_url;
   return imageEl;
 }
 
 function renderParagraphEl(articlesData) {
   const paragraphEl = document.createElement("p");
-  paragraphEl.innerText = articlesData.results[1].summary;
+  paragraphEl.innerText = articlesData.summary;
   return paragraphEl;
 }
 
