f4ef24ec8d66907d93881976ebb1dd82e827d682
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2025-06-13 22:55:39 +0200

    refactor: divide fetch function into smaller functions

--- script.js
+++ script.js
@@ -2,23 +2,41 @@ const endpoint = "https://api.spaceflightnewsapi.net/v4/articles";
 const bodyEl = document.querySelector("body");
 let articlesData = [];
 
-async function fetchArticlesData() {
+async function fetchAndRenderArticlesData() {
   const responseData = await fetch(endpoint, { method: "GET" });
   articlesData = await responseData.json();
 
-  console.log(articlesData.results);
+  //console.log(articlesData.results);
 
+  const header2El = renderHeader2El(articlesData);
+  const imageEl = renderImageEl(articlesData);
+  const paragraphEl = renderParagraphEl(articlesData);
+
+  renderArticle(header2El, imageEl, paragraphEl);
+}
+
+function renderArticle(header2El, imageEl, paragraphEl) {
   const articleEl = document.createElement("article");
-  const header2El = document.createElement("h2");
-  header2El.textContent = articlesData.results[1].title;
-  const imageEl = document.createElement("img");
-  imageEl.src = articlesData.results[0].image_url;
-  const paragraphEl = document.createElement("p");
-  paragraphEl.innerText = articlesData.results[1].summary;
-
   articleEl.append(header2El, imageEl, paragraphEl);
-
   bodyEl.appendChild(articleEl);
 }
 
-fetchArticlesData();
+function renderHeader2El(articlesData) {
+  const header2El = document.createElement("h2");
+  header2El.textContent = articlesData.results[1].title;
+  return header2El;
+}
+
+function renderImageEl(articlesData) {
+  const imageEl = document.createElement("img");
+  imageEl.src = articlesData.results[0].image_url;
+  return imageEl;
+}
+
+function renderParagraphEl(articlesData) {
+  const paragraphEl = document.createElement("p");
+  paragraphEl.innerText = articlesData.results[1].summary;
+  return paragraphEl;
+}
+
+fetchAndRenderArticlesData();
